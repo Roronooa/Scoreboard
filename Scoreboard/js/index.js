2c99e48a21b4c82ee591fa45bca9b26318a35b39
Roronooa <126214753+Roronooa@users.noreply.github.com>
2023-02-24 20:07:41 +0100

    Add files via upload

---
+++ Scoreboard/js/index.js
@@ -0,0 +1,13 @@
+let homeScore = document.getElementById("home-score");
+homeScore = 18;
+
+let awayScore = document.getElementById("away-score");
+awayScore = 10;
+
+let addBtn = document.getElementById("add-btn");
+addBtn = 1;
+
+function add1() {
+  let homeScore = addBtn;
+  homeScore.textContent += 1;
+}
